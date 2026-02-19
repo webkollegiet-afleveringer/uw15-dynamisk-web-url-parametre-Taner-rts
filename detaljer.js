d4a312d92d692993e7866206065bbfb82a3e19b1
TJ <tjshotta24@gmail.com>
2026-02-19 12:46:46 +0100

    done

--- detaljer.js
+++ detaljer.js
@@ -5,26 +5,55 @@ const mainwWapperDom = document.querySelector("#main-wrapper")
 const url = `./data/${id}.json`
 console.log(url);
 
-
 fetch(url)
-    .then((respons) => respons.json())
-    .then ((data) => {
-        createPaige(data);
+    .then(res => res.json())
+    .then(data => {
+        createPage(data);
+    })
+    .catch(err => console.error('Failed to load detail:', err));
 
-    })  
 
+function createPage(data) {
+    const {
+        image,
+        destination,
+        title,
+        subtitle,
+        text,
+        facilities = []
+    } = data;
 
-    function createPaige (data) {
+    const html = `
+        <div class="detalje-wrapper">
+            <div class="detalje-venstre">
+                <div class="image">
+                    ${image ? `<img src="img/${image}" alt="${title || destination}">` : ``}
+                    <button class="favorite-badge" aria-label="favorite">
+                        <svg width="13" height="12" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6.50034 12C6.70106 11.9997 6.89706 11.9391 7.06284 11.8259C9.51878 10.1587 10.5822 9.01562 11.1688 8.30094C12.4188 6.7775 13.0172 5.21344 13.0003 3.51969C12.9807 1.57875 11.4235 0 9.52909 0C8.15159 0 7.19753 0.775938 6.6419 1.42219C6.6243 1.44245 6.60255 1.4587 6.57813 1.46984C6.55371 1.48098 6.52718 1.48674 6.50034 1.48674C6.4735 1.48674 6.44697 1.48098 6.42255 1.46984C6.39812 1.4587 6.37638 1.44245 6.35878 1.42219C5.80315 0.775313 4.84909 0 3.47159 0C1.57721 0 0.0200272 1.57875 0.000339508 3.52C-0.0165358 5.21406 0.582526 6.77813 1.8319 8.30125C2.41846 9.01594 3.4819 10.1591 5.93784 11.8263C6.10366 11.9393 6.29965 11.9998 6.50034 12Z" fill="black"/>
+                        </svg>
+                        <span class="fav-label">FAVORIT</span>
+                    </button>
+                </div>
+            </div>
 
-        const {image,destination, title,} = data
-        // const image = data.image
-        // const destination    = data.destination
-        console.log(data.image);
+            <div class="detalje-højre">
+                <p class="location">${destination || ''}</p>
+                <h1 class="detalje-title">${title || ''}</h1>
+                ${subtitle ? `<h3 class="subtitle">${subtitle}</h3>` : ''}
+                ${text ? `<p class="description">${text}</p>` : ''}
 
-       const pageString=  `<h2>${title}</h2>`
+                ${facilities.length ? `<h4>Faciliteter</h4>
+                    <ul class="facilities">
+                        ${facilities.map(f => `<li>${f}</li>`).join('')}
+                    </ul>` : ''}
+            </div>
+        </div>
+    `;
 
-mainwWapperDom.insertAdjacentHTML("afterbegin", pageString)
-    }
+    mainwWapperDom.insertAdjacentHTML("afterbegin", html);
+}
+ 
 
 // console.log(pageSearch);
 // console.log(params.get("id"));
--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 
-const url = `./data/destinations.json`
+const url = `./data/destinations.json`;
+const mainWrapper = document.querySelector('#main-wrapper');
 
 fetch(url)
-    .then((respons) => respons.json())
-    .then ((data) => {
-        console.log (data)
+    .then(res => res.json())
+    .then(data => {
+        const destinations = data.destinations || [];
 
-    })  
+        const html = destinations.map(d => {
+            const {
+                id,
+                image,
+                destination,
+                title,
+                subtitle,
+                text,
+                facilities = []
+            } = d;
+
+            return `
+                                    <article class="card">
+                                            ${image ? `<a href="detalje.html?id=${id}"><img src="img/${image}" alt="${title || destination}"></a>` : ''}
+
+                                            <div class="card-footer">
+                                                <button class="heart-btn" aria-label="favorite">
+                                                    <svg width="13" height="12" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <path d="M6.50034 12C6.70106 11.9997 6.89706 11.9391 7.06284 11.8259C9.51878 10.1587 10.5822 9.01562 11.1688 8.30094C12.4188 6.7775 13.0172 5.21344 13.0003 3.51969C12.9807 1.57875 11.4235 0 9.52909 0C8.15159 0 7.19753 0.775938 6.6419 1.42219C6.6243 1.44245 6.60255 1.4587 6.57813 1.46984C6.55371 1.48098 6.52718 1.48674 6.50034 1.48674C6.4735 1.48674 6.44697 1.48098 6.42255 1.46984C6.39812 1.4587 6.37638 1.44245 6.35878 1.42219C5.80315 0.775313 4.84909 0 3.47159 0C1.57721 0 0.0200272 1.57875 0.000339508 3.52C-0.0165358 5.21406 0.582526 6.77813 1.8319 8.30125C2.41846 9.01594 3.4819 10.1591 5.93784 11.8263C6.10366 11.9393 6.29965 11.9998 6.50034 12Z" fill="black"/>
+                                                    </svg>
+                                                </button>
+
+                                                <a class="more-btn" href="detalje.html?id=${id}">MORE</a>
+                                            </div>
+                                    </article>
+                            `;
+        }).join('');
+
+        if (mainWrapper) mainWrapper.innerHTML = html;
+    })
